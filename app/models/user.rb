438c4334f880367843b0f1031a26f6dc3e228b0f
TakumiAbe <takuminn05015151@gmail.com>
2019-11-05 12:06:43 +0000

    first commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
-  validates :name, presence: true
-  validates :email, presence: true
+  validates :name, presence: true, length: { maximum: 15 }
+  validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :password, length: { in: 8..32 }, 
+                       format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}\z/i}
   
   has_secure_password
 end
