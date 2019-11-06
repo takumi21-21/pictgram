6e8d241b69a801d1f99a83e7fc0df1f675bcf3ef
TakumiAbe <takuminn05015151@gmail.com>
2019-11-06 08:16:22 +0000

    second commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 15 }
   validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   validates :password, length: { in: 8..32 }, 
-                       format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}\z/i}
+                       format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}+\z/i}
   
   has_secure_password
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get 'pages/help'
   root 'pages#index'
   
   resources :users
+  
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   
 end
