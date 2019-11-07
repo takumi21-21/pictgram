e8de53ac63f28354e44dd40e4b4785f7390876cd
TakumiAbe <takuminn05015151@gmail.com>
2019-11-07 07:05:50 +0000

    Image index

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
                        format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}+\z/i}
   
   has_secure_password
+  has_many :topics
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,13 @@ Rails.application.routes.draw do
   get 'pages/help'
   root 'pages#index'
   
-  resources :users
+  
   
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   
+  resources :users
+  resources :topics
+
 end
